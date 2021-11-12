406bda251649a7a0130ba30951aea58f3fcf5c9d
Breania Harvey <harvey.breania@gmail.com>
2021-11-12 01:53:05 -0600

    initial code added

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+
+// user can have many posts
+
+// post belongs to user
+
+// post can have many comments
+
+// comment belongs to post
+
+// users can have many comments
+
+// comment belongs to user
+
+module.exports = { User }
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const sequelize = require('./config/connection.js');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+const path = require('path');
+const routes = require('./controllers');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'I solemnly swear that I am up to no good.',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes)
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+});
