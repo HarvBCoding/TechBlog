df3206be87bcd9e19f23102da8c7f07efbadf45a
Breania Harvey <harvey.breania@gmail.com>
2021-11-13 00:01:38 -0600

    Comment model added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // user can have many posts
 User.hasMany(Post, {
@@ -12,11 +13,26 @@ Post.belongsTo(User, {
 });
 
 // post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 // comment belongs to post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
 
 // users can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
 // comment belongs to user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
-module.exports = { User, Post }
+module.exports = { User, Post, Comment }
