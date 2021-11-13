5b1b6bd20947a95f4c77a6cbbc3263162883a223
Breania Harvey <harvey.breania@gmail.com>
2021-11-12 23:43:20 -0600

    post routes added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -27,22 +27,95 @@ router.get('/', (req, res) => {
 
 // GET one post
 router.get('/:id', (req, res) => {
-    
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+      .then(postData => {
+          if (!postData) {
+              res.status(404).json({ message: 'No post found with this id'});
+              return;
+          }
+          res.json(postData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 // POST / create new user
 router.post('/', (req, res) => {
-
+    Post.create({
+        title: req.body.title,
+        post_text: req.body.post_text,
+        user_id: req.session.user_id
+    })
+      .then(postData => res.json(postData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 // PUT / update user
 router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            post_text: req.body.post_text
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
 
+    )
+      .then(postData => {
+          if (!postData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+          res.json(postData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 // DELETE / destroy user
 router.delete('/:id', (req, res) => {
-
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+      .then(postData => {
+          if (!postData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+          res.json(postData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 module.exports = router;
