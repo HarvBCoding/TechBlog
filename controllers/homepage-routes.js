10828b9bd7497b74221a28eb5c6950546d9553b7
Breania Harvey <harvey.breania@gmail.com>
2021-11-14 01:30:32 -0600

    login handlebar added

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -86,4 +86,13 @@ router.get('/post/:id', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+})
+
 module.exports = router;
