a811f63e98c857f94b6ddc7646ff042b94a9fd82
Breania Harvey <harvey.breania@gmail.com>
2021-11-14 23:51:36 -0600

    heroku error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -90,7 +90,9 @@ router.post('/logout', (req, res) => {
         req.session.destroy(() => {
             res.status(204).end();
         });
-    } 
+    } else {
+        res.status(404).end();
+    }
 });
 
 // PUT/update upser
